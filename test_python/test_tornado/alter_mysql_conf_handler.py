370fb28efb8ec4fc6c94e8021e2f5804036a4930
William <lhliu1994@gmail.com>
2016-09-28 17:46:24 +0800

    alter return value and form

--- test_python/test_tornado/alter_mysql_conf_handler.py
+++ test_python/test_tornado/alter_mysql_conf_handler.py
@@ -10,13 +10,15 @@ import tornado.options
 import tornado.web
 from tornado.httpclient import AsyncHTTPClient
 import json
+
 from utils.config_loader import load_log_config, load_mysql_config, save_config_to_redis
-from cache import KEY_TYPE_SYS_CONFIG
-from mysql_connmanager import MysqlConnManager
-from common.errors import MysqlDataError
+from common.errors import *
+from common.constants import RET_OK, RET_ERROR
+from engine.data.mysql.schema import *
+from engine.data.mysql.mysql_connmanager import MysqlConnManager
 
 logging.config.dictConfig(load_log_config())
-g_logger = logging.getLogger('handler_logger')
+g_logger = logging.getLogger('api_handler_logger')
 
 
 class AlterMysqlConfApiHandler(tornado.web.RequestHandler):
@@ -35,20 +37,53 @@ class AlterMysqlConfApiHandler(tornado.web.RequestHandler):
             param_mysql_db_name = self.get_argument('mysql_db_name', strip=False)
 
             # check if this db is exist
-            check_sign_for_db, api_return, mysql_conf = self.check_for_new_mysql_db(param_mysql_db_name)
+            check_sign_for_db, return_msg, mysql_conf = self.check_for_new_mysql_db(param_mysql_db_name)
 
             if check_sign_for_db:
                 self.alter_mysql_conf_in_redis(mysql_conf, param_mysql_db_name)
-                api_return += ' Update successfully.'
+                api_return = {
+                    'code': RET_OK,
+                }
             else:
-                api_return += ' Updata failed.'
+                api_return = {
+                    'code': RET_ERROR,
+                }
 
-        except IOError, e:
-            pass
+        except ApiArgumentMissingError as api_param_missing_error:
+            g_logger.error("api_missing_error, message:{0}".format(api_param_missing_error))
+            api_return = {
+                'code': RET_ERROR,
+                'error_code': ERR_CODE_MISSING_PARAMETER,
+                'error_msg': api_param_missing_error.message
+            }
+
+        except ApiArgumentTypeError as api_type_error:
+            g_logger.error("api_type_error, message:{0}".format(api_type_error))
+            api_return = {
+                'code': RET_ERROR,
+                'error_code': ERR_CODE_WRONG_PARAMETER_TYPE,
+                'error_msg': api_type_error.message
+            }
+
+        except ApiArgumentCheckSignError as api_check_sign_error:
+            g_logger.error("api_check_sign_error, message:{0}".format(api_check_sign_error))
+            api_return = {
+                'code': RET_ERROR,
+                'error_code': ERR_CODE_PARAMETER_SIGN_ERROR,
+                'error_msg': api_check_sign_error.message
+            }
+
+        except Exception as other_error:
+            g_logger.error("unknown error occur, message:{0}".format(other_error))
+            api_return = {
+                'code': RET_ERROR,
+                'error_code': ERR_CODE_UNKNOWN,
+                'error_msg': other_error.message
+            }
 
         finally:
             # send the result back
-            self.write(api_return)
+            self.write(json.dumps(api_return))
             self.finish()
 
     @tornado.web.asynchronous
@@ -72,7 +107,12 @@ class AlterMysqlConfApiHandler(tornado.web.RequestHandler):
             db=db_name,
             user=mysql_config['db_server']['user'],
             passwd=mysql_config['db_server']['password'],
+            use_unicode=mysql_config['common']['use_unicode'],
+            charset=mysql_config['common']['charset'],
+            connect_timeout=mysql_config['common']['connect_timeout'],
         )
+
+        # return error kinds
         if not mysql_conn:
             alter_info = 'New database does not exist.'
             return False, alter_info, mysql_config
