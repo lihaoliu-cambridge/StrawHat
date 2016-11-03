f369234c7e9ce40a8203bc7284c1bf90a0d13c09
William <lhliu1994@gmail.com>
2016-11-03 14:34:39 +0800

    reformat

--- leetcode_ac_questions/2.add_two_numbers.py
+++ leetcode_ac_questions/2.add_two_numbers.py
@@ -9,13 +9,16 @@ class Solution(object):
         tmp_head_node = l1
         final_node = tmp_head_node
         tmp_residual = 0
+
         while (l1 is not None) or (l2 is not None) or (tmp_residual == 1):
             if (tmp_residual == 1) and (l1 is None) and (l2 is None):
                 tmp_head_node = ListNode(1)
                 break
+
             tmp_value_1 = 0 if l1 is None else l1.val
             tmp_value_2 = 0 if l2 is None else l2.val
             tmp_sum = tmp_value_1 + tmp_value_2 + tmp_residual
+
             if tmp_sum >= 10:
                 if l1:
                     l1.val = tmp_sum % 10
@@ -28,11 +31,12 @@ class Solution(object):
                 if l2:
                     l2.val = tmp_sum
                 tmp_residual = 0
+
             if l1:
                 l1 = l1.next
                 tmp_head_node = l1
             if l2:
                 l2 = l2.next
                 tmp_head_node = l2
-        return final_node
 
+        return final_node
